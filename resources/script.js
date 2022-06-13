7721245971d9ee55c47bfdd111541e1b9245848a
IoIxD <alphaproject217@gmail.com>
2022-06-12 23:03:39 -0700

    what did i do uhh

--- resources/script.js
+++ resources/script.js
@@ -36,7 +36,12 @@ var properties;
 // can we use asynchronous code?
 var canAsync = true;
 
+// the ratio according to which to place objects on the screen
+var ratio; var desiredRatio;
+
 window.onload = function() {
+  // update the scale based on the device pixel ratio
+  scaleUpdate();
   // phone detection
   if(navigator.userAgent.match(/(iPad|iPhone|iPod|android)/i)) {
     onPhone = 1;
@@ -77,8 +82,8 @@ document.addEventListener("mousedown", function(e) {
           curZIndex = windows[i].style.zIndex;
           if(curZIndex == undefined) {
             curZIndex = 1;
-          } 
-          windows[i].style.zIndex = curZIndex-1;
+          }
+          if(curZIndex > 0) windows[i].style.zIndex = curZIndex-1;
         }
         // and push ours to the top
         if(elemHover[2].style.zIndex == undefined) {
@@ -113,7 +118,15 @@ document.addEventListener("mousedown", function(e) {
 
 document.addEventListener("mouseup", function() {
   mouseDown = 0; // our mouse is no longer down
-  movingWindow = 0; // and we're no longer moving a window, presumably
+  // if we were moving a window, stop moving it and deselect everything.
+  if(movingWindow == 1) {
+    for(var i = 0; i < windows.length; i++) {
+      windows[i].style.pointerEvents = 'initial';
+      windows[i].style.userSelect = 'initial';
+    }
+    window.getSelection().removeAllRanges();
+    movingWindow = 0;
+  }
 })
 
 // ctrl + alt + shift + o should bring the user to a plaintext version
@@ -134,42 +147,71 @@ document.addEventListener('keyup', function(e) {
     case "Control":   heldCtrl = 0;   break;
     case "Alt":       heldAlt = 0;    break;
     case "Shift":     heldShift = 0;  break;
-    case "O":         heldO = 0;      break;
+    case "O":         heldO = 0;      break;100 
   }
 })
 
 // window dragging 
 document.addEventListener("mousemove", function(e) {
-  if(mouseDown == 1) {
-    elemHover = document.querySelectorAll(`:hover`);
-    if(elemHover.length >= 3 && elemHover[2].classList[0] == "window") {
-      movingWindow = 1;
-      hoveredWin = elemHover[2];
+
+  // are we moving a window?
+  if(movingWindow) {
+    // if the window we're supposed to be moving is maximized then no we aren't.
+    if(hoveredWin.classList.contains("maximized")) {
+      return;
     }
-    if(movingWindow) {
-      hoveredWin.style.top = (+(e.pageY-my_o) + +wy_o)+"px";
-      hoveredWin.style.left = (+(e.pageX-mx_o) + +wx_o)+"px";
+    // move whatever window we're hovering over.
+    // first get what the position should be, in pixels.
+    newTop = (+(e.pageY-my_o) + +wy_o);
+    newLeft = (+(e.pageX-mx_o) + +wx_o);
+
+    // adjust it according to the window's width, converting it to a percentage.
+    newTop = (newTop / window.innerWidth)*100;
+    newLeft = (newLeft / window.innerHeight)*100;
+
+    // convert it back to pixels, again using the window's inner width.
+    newTop = window.innerWidth * (newTop)/100;
+    newLeft = window.innerHeight * (newLeft)/100;
+
+    hoveredWin.style.top = newTop+"px";
+    hoveredWin.style.left = newLeft+"px";
+
+    // disallow every window from being selected while we're moving the current one.
+    for(var i = 0; i < windows.length; i++) {
+      if(windows[i] != hoveredWin) {
+        windows[i].style.pointerEvents = 'none';
+        windows[i].style.userSelect = 'none';
+      }
+    }
+
+  } else {
+    // otherwise, check if we could be moving one, by checking if the mouse is down and we're over a window
+    if(mouseDown) {
+      elemHover = document.querySelectorAll(`:hover`);
+      if(elemHover.length >= 3 && elemHover[2].classList[0] == "window") {
+        movingWindow = 1;
+        hoveredWin = elemHover[2];
+      }
     }
   }
 })
 
-// return a populated string for the .content span of a window
-function contentStringPopulated(page,pageContents,width,height,left,top,options,title,titlebar_additions) {
-  return "<div id="+page+" style='display: block; z-index: 0; width: "+width+"; height: "+height+"; left:"+left+"; top:"+top+";' class='window "+options+"'>"+
-      "<span class='titlebar'>"+
-        "<span class='tl_lines'></span>"+
-        "<span onclick='windowRemove('"+page+"');' class='tl_button close'></span>"+
-        "<span class='title'>"+title+"</span>"+
-        "<span class='tl_button maxmin'></span>"+
-        "<span class='tl_button shade'></span>"+
-      "</span>"+
-      titlebar_additions+
-      "<span class='content "+options+"'>"+
-        pageContents+
-      "</span>"+
-    "</div";
-}
+window.addEventListener('resize', scaleUpdate);
 
+// function for handling zoom ins and zoom outs.
+// essentially we want to make sure the body can only ever scale evenly.
+function scaleUpdate() {
+  // set the scale of the body
+  var dpi = (document.querySelector('#dpi').offsetHeight / 96)-1;
+  ratio = window.devicePixelRatio;
+  desiredRatio = Math.floor(((ratio+dpi)-1)*10)+1; // how many tenths away is the ratio and dpi from 1?
+  if(desiredRatio % 2 != 0 && desiredRatio != 1) {desiredRatio++}; // make sure it's even, always.
+  if(desiredRatio <= 0) desiredRatio = 1; 
+  document.body.style.transform = "scale("+desiredRatio+")";
+  document.body.style.width = +(window.innerWidth/desiredRatio)-30+"px";
+  document.body.style.height = +(window.innerHeight/desiredRatio)+"px";
+  //document.body.style.height = "0px";
+}
 
 // window creation
 
@@ -219,19 +261,10 @@ function windowCreate(page, exoptions="") {
   var options = pageProperties.options;
   var title = properties[`${page}`].name;
 
-  // get all the windows on screen and catch them
   windows.length = 0;
   windows_r = document.getElementsByClassName("window");
   for ( i = 0; i < windows_r.length; i++ ) {
     windows.push(windows_r[i])
-  }
-
-  // for every cached window
-  for ( i = 0; i < windows.length; i++ ) {
-    // if it's not already set to not animate, do so
-    if(!windows[i].classList.contains('noanim')) {
-      windows[i].classList.add('noanim');
-    }
   }
 
   // if we're not phone and the window should animate, set that up.
@@ -270,20 +303,20 @@ function windowCreate(page, exoptions="") {
 
   // create a window based on what we got, using a different function based on your gender.
   if(canAsync) {
-    windowCreateFinalAsynchronous(page,width,height,left,top,options,title,titlebar_additions)
+    windowCreatePart2Asynchronous(page,width,height,left,top,options,title,titlebar_additions)
   } else {
-    windowCreateFinalSynchronous(page,width,height,left,top,options,title,titlebar_additions)
+    windowCreatePart2Synchronous(page,width,height,left,top,options,title,titlebar_additions)
   }
 }
 
 // the final part of the windowCreate function, asynchronously
-async function windowCreateFinalAsynchronous(page,width,height,left,top,options,title,titlebar_additions) {
+async function windowCreatePart2Asynchronous(page,width,height,left,top,options,title,titlebar_additions) {
   pageContents = await fetch(pageUrl).then(r => r.text())
-  document.body.innerHTML += contentStringPopulated(page,pageContents,width,height,left,top,options,title,titlebar_additions);
+  windowCreatePart3(page,pageContents,width,height,left,top,options,title,titlebar_additions);
 }
 
 // the final part of the windowCreate function, synchronously
-function windowCreateFinalynchronous(page,width,height,left,top,options,title,titlebar_additions) {
+function windowCreatePart2Synchronous(page,width,height,left,top,options,title,titlebar_additions) {
   try {
     var xhr = new XMLHttpRequest();
     xhr.open('Get', pageUrl, true);
@@ -297,12 +330,71 @@ function windowCreateFinalynchronous(page,width,height,left,top,options,title,ti
     console.error(ex);
     pageContents = "ERROR: <br>"+ex;
   }
-  document.body.innerHTML += contentStringPopulated(page,pageContents,width,height,left,top,options,title,titlebar_additions);
+  windowCreatePart3(page,pageContents,width,height,left,top,options,title,titlebar_additions);
+}
+
+function windowCreatePart3(page,pageContents,width,height,left,top,options,title,titlebar_additions) {
+  // create a div the proper way so that we can reference it
+  var div = document.createElement("div");
+  div.style.display =   "block";
+  div.style.width   =   width;
+  div.style.height  =   height;
+  div.style.left    =   left;
+  div.style.top     =   top;
+  div.id      =   page;
+  div.classList.add("window");
+  div.classList.add(options);
+  // create a div the improper way because fuck that
+  div.innerHTML = "<span class='titlebar'>"+
+                    "<span class='tl_lines'></span>"+
+                    "<span onclick='windowRemove(\""+page+"\");' class='tl_button close'></span>"+
+                    "<span class='title'>"+title+"</span>"+
+                    "<span onclick='windowShade(\""+page+"\");' class='tl_button shade'></span>"+
+                    "<span onclick='windowResize(\""+page+"\");' class='tl_button maxmin'></span>"+
+                  "</span>"+
+                  titlebar_additions+
+                  "<span class='content "+options+"'></span>"+
+                "</div";
+  windowPopulate(div,pageContents);
+  document.body.append(div);
+}
+
+// update the contents of a window.
+function windowPopulate(div,pageContents) {
+  var content = div.getElementsByClassName("content")[0];
+  if(typeof content === undefined) {
+    return;
+  }
+  content.innerHTML = pageContents;
 }
 
 // remove window
 function windowRemove(page) {
   document.getElementById(page).remove();
+}
+
+// resize window
+function windowResize(page) {
+  if(document.getElementById(page).classList.contains("maximized")) {
+    document.getElementById(page).classList.remove("maximized");
+    document.body.style.overflow = "auto";
+  } else {
+    document.getElementById(page).classList.add("maximized");
+    document.body.style.overflow = "hidden";
+  }
+
+}
+
+// shade window
+function windowShade(page) {
+  if(document.getElementById(page).classList.contains("shaded")) {
+    document.getElementById(page).classList.remove("shaded");
+    document.getElementById(page).style.marginTop = "0px";
+  } else {
+    document.getElementById(page).classList.add("shaded");
+    var shiftBy = +(document.getElementById(page).style.height.replace("px","",2))
+    document.getElementById(page).style.marginTop = -1*(shiftBy/2)+9+"px";
+  }
 }
 
 // quick and dirty function to open the three windows from the first icon, one after the other.
