09e8d70f4cbb4d0e548aaa61a7fa2ffff9434829
IoIxD <alphaproject217@gmail.com>
2022-07-20 21:32:39 -0700

    ah fucken hell the discord thing hasn't been working;\n not like it'll work anymore with the new structure

--- pages/main.php
+++ pages/main.php
@@ -4,19 +4,21 @@
         <link rel="stylesheet" type="text/css" href="../resources/about.css">
     </head>
     <body class='embed textfile'>
-<p><img class="broken" src="../images/broken.gif" width="20" height="22"> Hello!<br>
-<br>
-I am Gavin &ldquo;ioi_xd&rdquo; Parker, a 19 year old programmer, artist, and aspiring filmmaker. <br>
-I have several years of experience in HTML/CSS, and around a year or so of experience with advanced Javascript (and as such, Node.js) and I&rsquo;m currently learning Golang for my desktop and server applications. I&rsquo;m alright, albiet rusty, with PHP and Python.<br>
-In general, I really like technology, especially that of the older variety (1980-2002) but i consider myself a general fan. I&rsquo;m probably going to go into programming full time, but I&rsquo;d also like to make at least one film or comic in my life, as I also like doing 3d modelling and sometimes 2d art.<br>
-<br>
-In addition to this being my resume kind of, this is generally where I put all my dumb shit. In addition to serving as my carrd/listography (the actual sites don&rsquo;t give the customization i&rsquo;m looking for) this is where i put my art and sometimes programs.<br>
-<br>
-Feel free to look around here, or check out <a target="_blank" href="https://github.com/IoIxD/site">my github</a> to see what my main projects are and gauge the quality of my code. If you like any of the other things I like/do, we should be friends!<br>
-<br>
-<center><br>
-<b>hey check these sites out:</b><br>
-  <a target="_blank" href="https://modarchive.org/"><img alt="modarchive.org" src="https://content.pouet.net/buttons/tma_c.gif" border="0"></a><a target="_blank" href="https://web.archive.org/web/19961229152939im_/http://www.windows95.com/"><img src="https://web.archive.org/web/19961229152939im_/http://www2.windows95.com/gifs/w95com.gif" border="0"></a><a target="_blank" href="https://projectlounge.pw"><img  src="https://essem.space/images/projectloungenow.gif"></a><a target="_blank" href="https://gba.ioi-xd.net"><img src="https://media.discordapp.net/attachments/680549525902983194/963295134236180540/sherbetbutton.gif"></a></center></p>
+        <p>
+        <img class="broken" src="../images/broken.gif" width="20" height="22"> Hello!
+
+        I am Gavin "ioi_xd" Parker, a 20 year old programmer, artist, and aspiring filmmaker.
+        I have several years of experience in HTML/CSS, and around a year or so of experience with advanced Javascript (and as such, Node.js) and roughly a year with Golang though I am still coding with it to better learn how to best utilize it. I'm alright, albiet rusty, with PHP and Python.
+        In general, I really like technology, especially that of the older variety (1980-2002) but i consider myself a general fan. I'm probably going to go into programming full time, but I'd also like to make at least one film or comic in my life, as I also like doing 3d modelling and sometimes 2d art.
+
+        In addition to this being my resume (kind of), this is generally where I put all my dumb shit. In addition to serving as my carrd/listography (the actual sites don't give the customization i'm looking for) this is where i put my art and sometimes programs.
+
+        Feel free to look around here, or check out <a target="_blank" href="https://github.com/IoIxD/site">my github</a> to see what my main projects are and gauge the quality of my code. If you like any of the other things I like/do, we should be friends!
+
+        <center>
+        <b>hey check these sites out:</b>
+          <a target="_blank" href="https://modarchive.org/"><img alt="modarchive.org" src="https://content.pouet.net/buttons/tma_c.gif" border="0"></a><a target="_blank" href="https://web.archive.org/web/19961229152939im_/http://www.windows95.com/"><img src="https://web.archive.org/web/19961229152939im_/http://www2.windows95.com/gifs/w95com.gif" border="0"></a><a target="_blank" href="https://projectlounge.pw"><img  src="https://essem.space/images/projectloungenow.gif"></a><a target="_blank" href="https://gba.ioi-xd.net"><img src="https://media.discordapp.net/attachments/680549525902983194/963295134236180540/sherbetbutton.gif"></a></center>
+        </p>
 
 	</body>
 </html>
