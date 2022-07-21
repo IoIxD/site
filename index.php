d37cddebb4675cc76799d279e7ae2d61dbd505d5
IoIxD <alphaproject217@gmail.com>
2022-07-20 21:15:19 -0700

    removed the remenants of the async attempt

--- index.php
+++ index.php
@@ -4,26 +4,19 @@
 		/* this code is somewhat sluggish, but i think it's better then having the site appear on screen for a second before showing
 		the no-javascript error screen (if applicable). that said, text needs to be hidden and then shown with css as a result of this, to make the look less embaressing */
 
-		/* default to using async if we can */
 		var intended = location.origin+'/has_script.php';
-		try {
-			document.open();
-			var xhr = new XMLHttpRequest();
-			xhr.open('Get', intended, true);
-			xhr.onerror = function(e) {
-				console.log(e);
-			}
-			xhr.onload = function(e) {
-				console.log(xhr.responseText);
-				document.write(xhr.responseText);
-			}
-			document.close();
-		} catch(ex) {
-			console.error(ex);
-			document.open();
-			document.write("ERROR: <br>"+ex);
-			document.close();
+		document.open();
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', intended, true);
+		xhr.onerror = function(e) {
+			console.log(e);
 		}
+		xhr.onload = function(e) {
+			console.log(xhr.responseText);
+			document.write(xhr.responseText);
+		}
+		document.close();
+
 		</script>
 		<noscript><?php include('no_script.php');?></noscript>
 		<script>
