000ae66dc9335390f6351044a6cf3b9665df4259
IoIxD <alphaproject217@gmail.com>
2022-12-25 12:53:33 -0700

    uh

--- horseshit/index.php
+++ horseshit/index.php
@@ -5,15 +5,16 @@
 	<body>
 		<h1>ioi's personal benchmark.txt ("Horseshit™")</h1>
 		<p>the benchmark that wastes your time.</p>
-		<p>you will never render this scene in a timely manner. <b>I</b> will never render this scene in a timely manner. it loads 8192x8192 PNG quality textures onto a scene with a 16384x8192 skybox, with a ground that takes up 52,448,800 triangles, 26,224,400 faces, and 26,356,900 faces.</p>
+		<p>you will never render this scene in a timely manner. <strikethrough><b>I</b> will never render this scene in a timely manner.</strikethrough> it loads 8192x8192 PNG quality textures onto a scene with a 16384x8192 skybox, with a ground that takes up 52,448,800 triangles, 26,224,400 faces, and 26,356,900 faces.</p>
 		<p><em><a href='./br.png' target='_blank'>this is all really funny because the scene looks like shit. like legitimately this looks awful it's an eyesore you would not believe this takes as long as it does.</a></em></p>
-		<p>if you should actually fucking attempt this then i would hope you have a 255GB swapfile ready so that this can chew through the RAM you don't have.<br>
+		<p>if you should actually fucking attempt this then i would hope you have a 255GB swapfile ready so that this can chew through the RAM you don't have. if you get a faster time then me i'll post it<br>
 		unless otherwise stated, for all results the scene was rendered at 2048x1536, with 4096 samples.
 		</p>
 		<p><a href="./horeshit.tar.xz">download</a></p>
 		<h1>leaderboard</h1>
 		<table border="1">
 			<thead>
+				<td>person</td>
 				<td>date started</td>
 				<td>initialization time</td>
 				<td>samples rendered after 2 minutes</td>
@@ -25,16 +26,18 @@
 			</thead>
 			<tbody>
 			<tr>
+					<td>ioi</td>
 					<td>december 25th 2022</td>
 					<td>02:10.50</td> <!--- 02:10.50 --->
 					<td>272</td>
 					<td>5:01.89</td>
-					<td><b style='color: red'>CHANGE: 64GB</b></td>
+					<td><b style='color: red'>CHANGE: 64GB Memory</b></td>
 					<td>CPU</td>
 					<td>amd's linux drivers are shit i'm going to linux soon</td>
 					<td><b>2.80666666667</b></td>
 				</tr>
 				<tr>
+					<td>ioi</td>
 					<td>july 17th 2022</td>
 					<td><em>7 minutes</em></td>
 					<td><em>100</em></td>
