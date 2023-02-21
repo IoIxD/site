6fe78ed3449dd416a0edd36199c220e28b6926d0
IoIxD <alphaproject217@gmail.com>
2023-02-21 15:52:46 -0700

    found out how to show js in my top languages without it blowing out every other language i know

--- pages/top-languages.php
+++ pages/top-languages.php
@@ -1,1 +1,1 @@
-<img style='margin: 1%' width="98%" src='https://github-readme-stats.vercel.app/api/top-langs/?username=IoIxD&hide=javascript,java,c,cpp,makefile,html,css,idl,assembly,shell,hack&langs_count=10&exclude_repo=Psychopath' alt='Top Langs'>
+<img style='margin: 1%' width="98%" src='https://github-readme-stats.vercel.app/api/top-langs/?username=IoIxD&hide=java,c,cpp,makefile,html,css,idl,assembly,shell,hack&langs_count=10&exclude_repo=Psychopath,Funfun' alt='Top Langs'>
