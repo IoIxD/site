710877455806428876f4fe004e83cc99c09ba2b5
IoIxD <alphaproject217@gmail.com>
2023-02-23 12:20:02 -0700

    made the title lines a proper gradient to see if it would scale better

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 		<a href="https://wetdry.world/@IoIxD" rel="me"></a>
 	</head>
 	<body>
+		<noscript>
+			<h1>This site simulates an old operating system. Javascript is required.</h1>
+		</noscript>
 		<span style='display: none;' class='print'>
 			<?php include('print.php')?>
         </span>
