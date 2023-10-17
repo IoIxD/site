cd5898fcc38395e018f786c646df7a44eda1c791
IoIxD <alphaproject217@gmail.com>
2023-10-17 14:49:34 -0700

    more globs of buttons

--- resources/pages/main.php
+++ resources/pages/main.php
@@ -23,6 +23,62 @@
       <a target="_blank" href="https://projectlounge.pw"><img src="https://essem.space/images/projectloungenow.gif"></a>
       <a target="_blank" href="https://gba.ioi-xd.net"><img
           src="https://media.discordapp.net/attachments/680549525902983194/963295134236180540/sherbetbutton.gif"></a>
+	<div class="sq near 8831" style="max-width:780px">
+<p class="pstr poundme">either websites i like or just funny pictures!!!</p>
+<a href="/shrek2dvdIntro"><img alt="SHREK 2" src="images/shrek.png"></a>
+<a href="http://www.sheepit-renderfarm.com/?tag=Boxy_Boy" target="_blank"><img alt="SHEEPIT FREE DISTRIBUTED RENDER FARM" src="images/sheepitButtonTiny.gif"></a>
+<!-- <a href="http://shrekpod.pro/" target="_blank"><picture><source srcset="images/shrekpodProButton.webp" type="image/webp"><img alt="shrekpod pro" title="shrekpod pro" src=images/fb/shrekpodProButton.jpg></picture></a> --> <!-- goodbye shrekpod pro :( -->
+<a href="http://dimden.dev/" target="_blank"><img alt="DIMDEN" src="http://dimden.dev/services/images/88x31.gif"></a>
+<a href="http://invoxiplaygames.uk/" target="_blank"><img alt="InvoxiPlayGames" src="/images/ipg.png"></a>
+<img alt="THIS WEBSITE SUCKS" src="images/sucks2.gif" width="88px" height="31px">
+<a href="http://neocities.org/" target="_blank"><img alt="Neocities.org. THE WEB IS YOURS" src="images/neocities.gif"></a>
+<a href="http://toastytech.com/evil/index.html" target="_blank"><img alt="Internet Explorer is EVIL!" src="images/ieisevil.gif"></a>
+<a href="http://www.mozilla.org/en-US/firefox/browsers/" target="_blank"><picture><source srcset="images/ffox.webp" type="image/webp"><img src="images/fb/ffox.jpg" alt="Take back the web, Firefox"></picture></a>
+<img alt="Don't be a phone chump! Get a computer! NOW!" src="images/phonechump.gif">
+<img alt="Microsoff Internet Exploiter" src="images/ie4.gif">
+<a href="https://melps.neocities.org" target="_blank"><img alt="melps" title="melps" src="images/limk2.png" width="88px"></a>
+<img alt="BAN TIME TRAVEL &quot;NOW&quot;" src="images/timetravel.png">
+<a href="https://pocl.neocities.org" target="_blank"><img alt="pocl.v" src="https://pocl.neocities.org/img/poclbutton.gif"></a>
+<img alt="this site is MIKU APPROVED!" src="images/fubutton2.gif">
+<img alt="Google Chrome is EVIL!" src="images/chrome2.gif">
+<a href="https://roachette.neocities.org"><img src="https://roachette.neocities.org/rchettebutton.gif" alt="ROACH STARZ"></a>
+<a href="https://uncannyvalley.neocities.org/" target="_blank"><img alt="UNCANNY VALLEY WEB" src="https://uncannyvalley.neocities.org/uncanny.gif"></a> 
+<img src="images/bestview.gif" alt="Best viewed at 800x600">
+<a href="https://bungeegum.neocities.org/" target="_blank"><img alt="BUNGEE GUM" src="https://cdn.discordapp.com/attachments/623616018350211092/890270869853732924/button.gif"></a>
+<a href="https://cyber.dabamos.de/88x31" target="_blank"><img alt="88x31" src="https://maia.crimew.gay/badges/88x31.gif"></a>
+<img alt="Piracy Now! FREE" src="images/piracy.png">
+<img alt="DOOM" src="images/doomrl.gif">
+<img alt="DESPACITO Now!" src="images/desp-anim.gif">
+<a href="https://neocities.org" target="_blank"><img alt="DELETE TWITTER, MAKE A NEOCITIES" src="images/delete-twitter.gif"></a>
+<img alt="POWERED BY Darwin" src="images/darwin.gif">
+<img alt="Best Viewed 800x600" src="images/800x600a.gif">
+<img alt="I wish it was 2001!" src="images/2001.gif">
+<img alt="Still using buttons in 2019!" src="images/2019.gif">
+<img alt="ACAB" src="images/acab.gif">
+<img alt="NOT MY PRES, awful drawing of joe biden shitting" src="images/bidShartButton.jpg">
+<picture><source srcset="images/beyes.webp" type="image/webp"><img src="images/beyes.gif" alt="no clue"></picture>
+<picture><source srcset="images/bob.webp" type="image/webp"><img src="images/bob.gif" alt="POWERED BY bob"></picture>
+<a href="https://youtu.be/5t53TcKIlMc" target="_blank"><img alt="d-d-doin your mom" src="images/doingyourmom.gif"></a>
+<img alt="Dreamcast" src="images/dreamcast.gif">
+<img alt="Play With STEAM" src="images/steam.gif">
+<a href="https://skidzone.neocities.org" target="_blank"> <img alt="SKIDZONE" src="https://skidzone.neocities.org/assets/site.png"></a>
+<a href="https://youtu.be/Sku4_2WHUcw?t=30" target="_blank"><picture><source srcset="images/hhButton.webp" type="image/webp"><img src="images/hhButton.gif" alt="jesse, the line was &quot;im sorry mr white&quot;"></picture></a>
+<a href="https://boogtoons.neocities.org/" target="_blank"><img alt="BOOGTOONS" src="https://boogtoons.neocities.org/PinkButton.jpg"></a>
+<a href="https://nansensu.neocities.org" target="_blank"><img alt="NANSENSU" src="https://boogtoons.neocities.org/rutaabutton.png"></a>
+<a href="https://twelvemen.neocities.org/index.html" target="_blank"><img alt="TWELVE MEN" src="https://twelvemen.neocities.org/12men.gif"></a>
+<a href="https://umjammercammy.neocities.org" target="_blank"><img alt="UMJAMMERCAMMY" src="https://umjammercammy.neocities.org/buttons/ujcbutton.gif"></a>
+<img src="/images/FirtniteButton.png" alt="FIRTNITE">
+<a href="https://melankorin.net/"><img src="https://melankorin.net/assets/img/buttons/button-1.gif" alt="melankorin"></a>
+<a href="https://sillykitty136.neocities.org/homepage" target="_blank"><picture><source srcset="images/sillykitty.webp" type="image/webp"><img src="https://sillykitty136.neocities.org/Button.gif" alt="silly kitty 136"></picture></a>
+<a href="https://smokepowered.com" target="_blank"><img src="images/smoke.gif" alt="SMOKE powered"></a>
+<script>
+document.write("<img src='images/js-free.png' alt='JAVASCRIPT FREE PAGE'>");
+</script><img src="images/js-free.png" alt="JAVASCRIPT FREE PAGE">
+<a href="https://epicblazed.com" target="_blank"><img src="images/EpicBlazedButton.png" alt="Epic Blazed"></a>
+
+<a href="https://ioletsgo.gay" target="_blank"><img src="Assets/buttons/ivorybutton.gif"></a> <a href="https://squibbus.neocities.org/"><img src="Assets/buttons/axelbutton.gif"></a> <a href="https://nicox.neocities.org/"><img src="Assets/buttons/nicox.png"></a> <a href="https://danielah05.github.io/"><img src="Assets/buttons/daniela.gif"></a> <a href="https://c7.pm/"><img src="Assets/buttons/c7-badge.png"></a> <a href="https://radixcomet.neocities.org/"><img src="Assets/buttons/radixbutton.png"></a> <a href="https://nixgoat.me/"><img src="Assets/buttons/nixgoatbutton.gif"></a> <a href="https://invoxiplaygames.uk/"><img src="Assets/buttons/ipg.png"></a> <a href="https://thxdee.neocities.org/"><img src="Assets/buttons/thxdee.gif"></a>
+
+</div>
     </center>
   </noprint>
 </p>
