58b52204bc709a9b426762b0b7b5def4f1f13e64
IoIxD <30945097+IoIxD@users.noreply.github.com>
2023-11-26 19:23:27 -0700

    Update dndcalc.php

--- resources/pages/dndcalc.php
+++ resources/pages/dndcalc.php
@@ -1,1 +1,13 @@
-<input type="number" />
+<input type="number" id="one"/>
+<input type="number" id="two"/>
+
+<span id="result"></span>
+<script>
+  function calculate(e) {
+    let a = +(document.querySelector("#one").value);
+    let d = +(document.querySelector("#two").value);
+    document.querySelector("#result").innerHTML = +(((a/d)/5)+1);
+  }
+    document.querySelector("#one").addEventListener("change", calculate);
+  document.querySelector("#two").addEventListener("change", calculate);
+</script>
