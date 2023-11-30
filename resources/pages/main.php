f37e3bd908dd5cbef12e3e99ebb7eb6c6c6e1f45
IoIxD <alphaproject217@gmail.com>
2023-11-30 10:07:39 -0700

    changed my mind, i don't want all those buttons

--- resources/pages/main.php
+++ resources/pages/main.php
@@ -23,51 +23,6 @@
       <a target="_blank" href="https://projectlounge.pw"><img src="https://essem.space/images/projectloungenow.gif"></a>
       <a target="_blank" href="https://gba.ioi-xd.net"><img
           src="https://media.discordapp.net/attachments/680549525902983194/963295134236180540/sherbetbutton.gif"></a>
-      <a href="https://boxy.neocities.org/shrek2dvdIntro"><img alt="SHREK 2" src="https://boxy.neocities.org/images/shrek.png"></a>
-      <a href="http://www.sheepit-renderfarm.com/?tag=Boxy_Boy" target="_blank"><img alt="SHEEPIT FREE DISTRIBUTED RENDER FARM" src="https://boxy.neocities.org/images/sheepitButtonTiny.gif"></a>
-      <a href="http://dimden.dev/" target="_blank"><img alt="DIMDEN" src="http://dimden.dev/services/images/88x31.gif"></a>
-      <a href="http://invoxiplaygames.uk/" target="_blank"><img alt="InvoxiPlayGames" src="https://boxy.neocities.org/images/ipg.png"></a>
-      <img alt="THIS WEBSITE SUCKS" src="https://boxy.neocities.org/images/sucks2.gif" width="88px" height="31px">
-      <a href="http://neocities.org/" target="_blank"><img alt="Neocities.org. THE WEB IS YOURS" src="https://boxy.neocities.org/images/neocities.gif"></a>
-      <a href="http://toastytech.com/evil/index.html" target="_blank"><img alt="Internet Explorer is EVIL!" src="https://boxy.neocities.org/images/ieisevil.gif"></a>
-      <a href="http://www.mozilla.org/en-US/firefox/browsers/" target="_blank"><picture><source srcset="images/ffox.webp" type="image/webp"><img src="https://boxy.neocities.org/images/fb/ffox.jpg" alt="Take back the web, Firefox"></picture></a>
-      <img alt="Don't be a phone chump! Get a computer! NOW!" src="https://boxy.neocities.org/images/phonechump.gif">
-      <img alt="Microsoff Internet Exploiter" src="https://boxy.neocities.org/images/ie4.gif">
-      <a href="https://melps.neocities.org" target="_blank"><img alt="melps" title="melps" src="https://boxy.neocities.org/images/limk2.png" width="88px"></a>
-      <img alt="BAN TIME TRAVEL &quot;NOW&quot;" src="https://boxy.neocities.org/images/timetravel.png">
-      <a href="https://pocl.neocities.org" target="_blank"><img alt="pocl.v" src="https://pocl.neocities.org/img/poclbutton.gif"></a>
-      <img alt="this site is MIKU APPROVED!" src="https://boxy.neocities.org/images/fubutton2.gif">
-      <img alt="Google Chrome is EVIL!" src="https://boxy.neocities.org/images/chrome2.gif">
-      <a href="https://roachette.neocities.org"><img src="https://roachette.neocities.org/rchettebutton.gif" alt="ROACH STARZ"></a>
-      <a href="https://uncannyvalley.neocities.org/" target="_blank"><img alt="UNCANNY VALLEY WEB" src="https://uncannyvalley.neocities.org/uncanny.gif"></a> 
-      <img src="https://boxy.neocities.org/images/bestview.gif" alt="Best viewed at 800x600">
-      <a href="https://bungeegum.neocities.org/" target="_blank"><img alt="BUNGEE GUM" src="https://cdn.discordapp.com/attachments/623616018350211092/890270869853732924/button.gif"></a>
-      <a href="https://cyber.dabamos.de/88x31" target="_blank"><img alt="88x31" src="https://maia.crimew.gay/badges/88x31.gif"></a>
-      <img alt="Piracy Now! FREE" src="https://boxy.neocities.org/images/piracy.png">
-      <img alt="DOOM" src="https://boxy.neocities.org/images/doomrl.gif">
-      <img alt="DESPACITO Now!" src="https://boxy.neocities.org/images/desp-anim.gif">
-      <a href="https://neocities.org" target="_blank"><img alt="DELETE TWITTER, MAKE A NEOCITIES" src="https://boxy.neocities.org/images/delete-twitter.gif"></a>
-      <img alt="POWERED BY Darwin" src="https://boxy.neocities.org/images/darwin.gif">
-      <img alt="Best Viewed 800x600" src="https://boxy.neocities.org/images/800x600a.gif">
-      <img alt="I wish it was 2001!" src="https://boxy.neocities.org/images/2001.gif">
-      <img alt="Still using buttons in 2019!" src="https://boxy.neocities.org/images/2019.gif">
-      <img alt="ACAB" src="https://boxy.neocities.org/images/acab.gif">
-      <img alt="NOT MY PRES, awful drawing of joe biden shitting" src="https://boxy.neocities.org/images/bidShartButton.jpg">
-      <picture><source srcset="images/beyes.webp" type="image/webp"><img src="https://boxy.neocities.org/images/beyes.gif" alt="no clue"></picture>
-      <picture><source srcset="images/bob.webp" type="image/webp"><img src="https://boxy.neocities.org/images/bob.gif" alt="POWERED BY bob"></picture>
-      <a href="https://youtu.be/5t53TcKIlMc" target="_blank"><img alt="d-d-doin your mom" src="https://boxy.neocities.org/images/doingyourmom.gif"></a>
-      <img alt="Dreamcast" src="https://boxy.neocities.org/images/dreamcast.gif">
-      <img alt="Play With STEAM" src="https://boxy.neocities.org/images/steam.gif">
-      <a href="https://skidzone.neocities.org" target="_blank"> <img alt="SKIDZONE" src="https://skidzone.neocities.org/assets/site.png"></a>
-      <a href="https://youtu.be/Sku4_2WHUcw?t=30" target="_blank"><picture><source srcset="images/hhButton.webp" type="image/webp"><img src="https://boxy.neocities.org/images/hhButton.gif" alt="jesse, the line was &quot;im sorry mr white&quot;"></picture></a>
-      <a href="https://boogtoons.neocities.org/" target="_blank"><img alt="BOOGTOONS" src="https://boogtoons.neocities.org/PinkButton.jpg"></a>
-      <a href="https://nansensu.neocities.org" target="_blank"><img alt="NANSENSU" src="https://boogtoons.neocities.org/rutaabutton.png"></a>
-      <a href="https://twelvemen.neocities.org/index.html" target="_blank"><img alt="TWELVE MEN" src="https://twelvemen.neocities.org/12men.gif"></a>
-      <a href="https://umjammercammy.neocities.org" target="_blank"><img alt="UMJAMMERCAMMY" src="https://umjammercammy.neocities.org/buttons/ujcbutton.gif"></a>
-      <img src="https://boxy.neocities.org/images/FirtniteButton.png" alt="FIRTNITE">
-      <a href="https://melankorin.net/"><img src="https://melankorin.net/assets/img/buttons/button-1.gif" alt="melankorin"></a>
-      <a href="https://sillykitty136.neocities.org/homepage" target="_blank"><picture><source srcset="images/sillykitty.webp" type="image/webp"><img src="https://sillykitty136.neocities.org/Button.gif" alt="silly kitty 136"></picture></a>
-      <a href="https://smokepowered.com" target="_blank"><img src="https://boxy.neocities.org/images/smoke.gif" alt="SMOKE powered"></a>
       <script>
       document.write("<img src='images/js-free.png' alt='JAVASCRIPT FREE PAGE'>");
       </script><img src="https://boxy.neocities.org/images/js-free.png" alt="JAVASCRIPT FREE PAGE">
